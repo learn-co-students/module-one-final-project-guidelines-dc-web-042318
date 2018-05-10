ef9e8d8a2a7bfe79eb712de0fcdf872442f883b6
Adam Cohn <adamcohn88@gmail.com>
2018-05-10 18:00:41 -0400

    done for the day

--- bin/run.rb
+++ bin/run.rb
@@ -1,11 +1,6 @@
 require_relative '../config/environment'
 
-def test_search
-  puts "Search for an individual"
-  input = gets.chomp
-  search = Person.search_individual(input)
-  search.each_with_index { |n, i| puts "#{i + 1}. #{n.name}     #{n.title}" }
-end
+
 
 CLI_Functions.new
 
